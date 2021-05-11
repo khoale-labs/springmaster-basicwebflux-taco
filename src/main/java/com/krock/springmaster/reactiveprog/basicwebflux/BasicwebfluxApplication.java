46eeeb0c62fced4f9282f0b6f46f7fd1f5616db2
Khoa Le <khoale@KHOALE-MBP.local>
2021-05-11 11:29:51 +0700

    Initial

---
+++ src/main/java/com/krock/springmaster/reactiveprog/basicwebflux/BasicwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.krock.springmaster.reactiveprog.basicwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicwebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicwebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krock/springmaster/reactiveprog/basicwebflux/BasicwebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krock.springmaster.reactiveprog.basicwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicwebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
